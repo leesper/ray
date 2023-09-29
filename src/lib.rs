23494b992aa9f9e4b23c0ce68d0844725ef2aa67
leesper <pascal7718@gmail.com>
2023-09-29 16:44:38 +0800

    feat: Design tcp echo server example

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,19 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+// design: tcp echo server:
+// use log::{info};
+// let server = TcpServer::new()
+// .bind(1234)
+// .onConnect(|conn: &Conn| {
+//      info!("{} connected", conn);
+// })
+// .onData(|conn: &mut Conn, buf: &Buffer| {
+//      info!("received: {}", but.to_string());
+//      conn.send(buf.to_string());
+// });
+// server.serve();
+
 #[cfg(test)]
 mod tests {
     use super::*;
